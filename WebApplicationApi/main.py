1c30eed4394563c2cf5df09f55e66727a001da22
jpbsa98 <jpb.sa@hotmail.com>
2020-04-27 17:25:05 +0100

    website atualizado

--- WebApplicationApi/main.py
+++ WebApplicationApi/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     dados = data.Global()
-    return render_template('main.html', text = None)
+    return render_template('index.html', value = max(data.confirmados_total),value2 = max(data.recuperados_total),value3 = max(data.mortes_total),value4 = max(data.confirmados_total)-max(data.recuperados_total)- max(data.mortes_total))
 
 
 @app.route('/Country')
